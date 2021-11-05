524221854e0b67afcb235738b9f6f72c98d131a6
Lucas Tolón Pachecho <tolon.pacheco.lucas@alumnat.copernic.cat>
2021-11-05 10:52:09 +0100

    navmenu-login.kt-gradle

--- app/src/main/java/cat/copernic/donate/ui/login/LoginActivity.kt
+++ app/src/main/java/cat/copernic/donate/ui/login/LoginActivity.kt
@@ -3,12 +3,19 @@ package cat.copernic.donate.ui.login
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.DataBindingUtil.*
 import cat.copernic.donate.R
+import cat.copernic.donate.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
+        val binding = DataBindingUtil.setContentView<FragmentTitleBinding>(this, R.layout.activity_main)
+
     }
 }
