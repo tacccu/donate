b86e45a5aa9fb448576b5b3865b6295189a90e5b
Lucas Tolón Pachecho <tolon.pacheco.lucas@alumnat.copernic.cat>
2021-11-09 11:49:27 +0100

    Menu Nav Graph (a medias)

--- app/src/main/java/cat/copernic/donate/ui/login/LoginActivity.kt
+++ app/src/main/java/cat/copernic/donate/ui/login/LoginActivity.kt
@@ -6,30 +6,43 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import cat.copernic.donate.R
 import cat.copernic.donate.databinding.ActivityLoginBinding
+//import cat.copernic.donate.navigation.databinding.ActivityMainBinding
 import cat.copernic.donate.ui.registro.RegistroActivity
 
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
+    private lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+        //val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_login)
 
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        //drawerLayout = binding.drawerLayout
 
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        //NavigationUI.setupWithNavController(binding.navView, navController)
         //binding.RegisterButton.setOnClickListener {
         //    Log.i("LoginActivity", "He clicado boton registro")
         //    intent = Intent(this, RegistroActivity::class.java)
         //    startActivity(intent)
         //}
 
-
+        //Menu start here
+        //NavigationUI.setupWithNavController(binding.navView, navController)
 
     }
 
-
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return NavigationUI.navigateUp(navController, drawerLayout)
+    }
 }
