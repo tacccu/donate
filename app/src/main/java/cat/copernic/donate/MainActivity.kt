0cf92904f2c2d90147eff078a8c4476dffcdbf75
Lucas Tolón Pachecho <tolon.pacheco.lucas@alumnat.copernic.cat>
2021-11-16 10:11:01 +0100

    progres

--- app/src/main/java/cat/copernic/donate/MainActivity.kt
+++ app/src/main/java/cat/copernic/donate/MainActivity.kt
@@ -1,29 +1,37 @@
 package cat.copernic.donate
 
 import android.os.Bundle
-import android.widget.Toolbar
-import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
-import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
-import cat.copernic.donate.databinding.ActivityLoginBinding
-import com.google.android.material.navigation.NavigationView
-
-private lateinit var drawerLayout: DrawerLayout
+import cat.copernic.donate.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityLoginBinding
     lateinit var drawerLayout: DrawerLayout
-    lateinit var navigationView: NavigationView
-    lateinit var toolBar: Toolbar
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_tldonaciones)
 
+        //Menu
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(
+            this,
+            R.layout.activity_main
+        )
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        drawerLayout = binding.drawerLayout
+        NavigationUI.setupActionBarWithNavController(this, navController)
+        NavigationUI.setupWithNavController(binding.navView, navController)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return NavigationUI.navigateUp(navController, drawerLayout)
     }
 }
