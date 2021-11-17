11d56a70943ee023ef6807aa8247a854f6eda278
Lucas Tolón Pachecho <tolon.pacheco.lucas@alumnat.copernic.cat>
2021-11-17 10:55:45 +0100

    Conexión con Firebase

--- app/src/main/java/cat/copernic/donate/MainActivity.kt
+++ app/src/main/java/cat/copernic/donate/MainActivity.kt
@@ -17,35 +17,6 @@ import cat.copernic.donate.databinding.FragmentTldonacionesBinding
 
 private lateinit var drawerLayout: DrawerLayout
 
-/*class MainActivity : Fragment() {
-    private lateinit var drawerLayout: DrawerLayout
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        val binding: FragmentTldonacionesBinding = DataBindingUtil.inflate(
-            inflater,
-            R.layout.fragment_tldonaciones,
-            container,
-            false
-        )
-        super.onCreate(savedInstanceState)
-
-        //Menu
-        //val bindingActMain = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
-        //drawerLayout = bindingActMain.drawerLayout
-        //val navController = this.findNavController(R.id.myNavHostFragment)
-        /*NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
-        NavigationUI.setupWithNavController(bindingActMain.navView, navController)*/
-        return binding.root
-    }
-
-  /*  override fun onSupportNavigateUp(): Boolean {
-        val navController = this.findNavController(R.id.myNavHostFragment)
-        return NavigationUI.navigateUp(navController, drawerLayout)
-    }*/
-}*/
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState : Bundle?){
         super.onCreate(savedInstanceState)
