765ef69e04118ea053196cbf581d61d97237701f
pedro <algarra.delgado.pedro@alumnat.copernic.cat>
2021-11-19 08:42:24 +0100

    programa vuelve a funcionar + restructurado

--- app/src/main/java/cat/copernic/donate/ui/MainActivity.kt
+++ app/src/main/java/cat/copernic/donate/ui/MainActivity.kt
@@ -5,7 +5,9 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil.setContentView
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import cat.copernic.donate.R
 import cat.copernic.donate.databinding.ActivityMainBinding
@@ -19,7 +21,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState : Bundle?){
         super.onCreate(savedInstanceState)
         binding = setContentView(this, R.layout.activity_main)
-        val navController = this.findNavController(R.id.myNavHostFragment)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.myNavHostFragment) as NavHostFragment
+        val navController : NavController = navHostFragment.navController
 
         drawerLayout = binding.drawerLayout
         NavigationUI.setupActionBarWithNavController(this, navController)
