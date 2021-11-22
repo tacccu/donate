43b5dbf9b2664debd51c1f7a550006772e233051
pedro <algarra.delgado.pedro@alumnat.copernic.cat>
2021-11-22 10:22:57 +0100

    spinners fragment_perfil.xml y activity_registro.xml estilizados (fragment_perfil tuvo que ser reparado)

--- app/src/main/java/cat/copernic/donate/ui/Perfil.kt
+++ app/src/main/java/cat/copernic/donate/ui/Perfil.kt
@@ -1,10 +1,14 @@
 package cat.copernic.donate.ui
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import androidx.fragment.app.FragmentActivity
 import cat.copernic.donate.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -34,8 +38,18 @@ class Perfil : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val t = inflater.inflate(R.layout.fragment_perfil, container, false)
+        val spinner = t.findViewById<Spinner>(R.id.spinnerSelecCuentaPerfil)
+        spinner.adapter = context?.let {
+            ArrayAdapter(
+                it,
+                R.layout.spinner_dropdown_item,
+                resources.getStringArray(R.array.tipoCuenta)
+            )
+        }
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_perfil, container, false)
+        return t
     }
 
     companion object {
