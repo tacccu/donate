3b276e4bef2a34d9b3daf270f1e734130b093ef8
Lucas Tolón Pachecho <tolon.pacheco.lucas@alumnat.copernic.cat>
2021-12-02 09:20:33 +0100

    ---

--- app/src/main/java/cat/copernic/donate/ui/adapters/adapter.kt
+++ app/src/main/java/cat/copernic/donate/ui/adapters/adapter.kt
@@ -1,37 +1,16 @@
 package cat.copernic.donate.ui.adapters
 
-import android.content.Context
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.view.menu.ActionMenuItemView
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.RecyclerViewAccessibilityDelegate
-import cat.copernic.donate.ui.DonacionesList
+import android.widget.Toast
 import cat.copernic.donate.ui.model.donacion
 
-class adapter: RecyclerView.Adapter<adapter.ViewHolder>() {
-    var donaciones : MutableList<donacion> = ArrayList()
-    lateinit var context: Context
+class adapter(private val donaciones : ArrayList<donacion>) : RecyclerView.Adapter<adapter.MyViewHolder>(){
 
-    fun donacionesRecycle(donacionesList: MutableList<donacion>, contxt: Context){
-        this.donaciones = donacionesList
-        this.context = contxt
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): adapter.ViewHolder {
-        val layoutInflater = LayoutInflater.from(parent.context)
-        return RecyclerView.ViewHolder(
-            DonacionesListBinding.inflate(
-                layoutInflater, parent, false
-            )
-        )
-    }
-
-    override fun onBindViewHolder(holder: adapter.ViewHolder, position: Int) {
-
-        with(holder){
-            with(donaciones.get(position)){
-                binding.
-            }
-        }
-    }
 }
--- app/src/main/java/cat/copernic/donate/ui/registro/creaPost.kt
+++ app/src/main/java/cat/copernic/donate/ui/registro/creaPost.kt
@@ -23,7 +23,8 @@ private const val ARG_PARAM2 = "param2"
 class creaPost : Fragment() {
 
     private val db = FirebaseFirestore.getInstance()
-    private lateinit var binding : creaPostBinding
+
+
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -44,7 +45,7 @@ class creaPost : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         /*return inflater.inflate(R.layout.fragment_crea_post, container, false)*/
-
+        binding = creaPost.inflate()
         /*//val t = inflater.inflate(R.layout.fragment_perfil, container, false)
         val fab = findViewById<FloatingActionButton>(R.id.floatingActionButton)
 
