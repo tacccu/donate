f4552acf43edcdb1ad92bff4b9df49a01bcebb2e
pedro <algarra.delgado.pedro@alumnat.copernic.cat>
2021-12-02 10:18:51 +0100

    almost success edit perfil

--- app/src/main/java/cat/copernic/donate/ui/Perfil.kt
+++ app/src/main/java/cat/copernic/donate/ui/Perfil.kt
@@ -11,12 +11,15 @@ import cat.copernic.donate.R
 import cat.copernic.donate.databinding.FragmentPerfilBinding
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.auth.ktx.userProfileChangeRequest
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 
 
 class Perfil : Fragment() {
     private var __binding: FragmentPerfilBinding? = null
     private val binding get() = __binding!!
+    private var db = FirebaseFirestore.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -34,19 +37,24 @@ class Perfil : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val user = Firebase.auth.currentUser
+        val uid = user?.getEmail()
 
         binding.sendUpdateButton.setOnClickListener() {
-            if (user != null && binding.editTextPhone.text.isNotEmpty()) {
-                val profileUpdates = userProfileChangeRequest {
-                    var numTelef = binding.editTextPhone
-                }
+            if (user != null ) {
+                val userUpdNumTelf = db.collection("usuarios").document(uid.toString())
 
-                user!!.updateProfile(profileUpdates)
-                    .addOnCompleteListener { task ->
-                        if (task.isSuccessful) {
-                            Log.d(tag, "User profile updated.")
-                        }
-                    }
+                if(binding.editTextTextPersonName5.text.toString().isNotEmpty()){
+                    userUpdNumTelf.update("usuario", binding.editTextTextPersonName5.text.toString())
+                }
+                /*if(binding.editTextTextEmailAddress.text.toString().isNotEmpty()){
+                    user!!.updateEmail(binding.editTextTextEmailAddress.text.toString())
+                }*/
+                if(binding.editTextPhone.text.toString().isNotEmpty()){
+                    userUpdNumTelf.update("numTelef", binding.editTextPhone.text.toString())
+                }
+                /*if(binding.editTextPhone.text.toString().isNotEmpty()){
+                    userUpdNumTelf.update("numTelef", binding.spinnerSelecCuentaPerfil.get)
+                }*/
             }
         }
 
