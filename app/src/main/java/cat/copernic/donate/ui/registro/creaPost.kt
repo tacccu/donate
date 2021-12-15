179bb28f2fe4cc0e67b60ff86786b2fc62eab994
Lucas Tolón Pachecho <tolon.pacheco.lucas@alumnat.copernic.cat>
2021-12-15 10:39:36 +0100

    creaPost abre camara || galeria

--- app/src/main/java/cat/copernic/donate/ui/registro/creaPost.kt
+++ app/src/main/java/cat/copernic/donate/ui/registro/creaPost.kt
@@ -1,13 +1,20 @@
 package cat.copernic.donate.ui.registro
 
+import android.app.Activity
 import android.app.AlertDialog
+import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
 import android.widget.ImageView
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.FileProvider
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.findNavController
 import cat.copernic.donate.R
 import cat.copernic.donate.databinding.FragmentCreaPostBinding
@@ -15,6 +22,7 @@ import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.StorageReference
 import java.io.ByteArrayOutputStream
+import java.io.File
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -29,8 +37,19 @@ private const val ARG_PARAM2 = "param2"
 class creaPost : Fragment() {
 
     private val db = FirebaseFirestore.getInstance()
-    lateinit var imagen : ImageView
+    lateinit var imagen: ImageView
     lateinit var storageRef: StorageReference
+    lateinit var binding: FragmentCreaPostBinding
+    private var latestTempUri: Uri? = null
+
+    val tomarImgResult =
+        registerForActivityResult(ActivityResultContracts.TakePicture()) { isSucces ->
+            if (isSucces) {
+                latestTempUri?.let { uri ->
+                    binding.imageView6.setImageURI(uri)
+                }
+            }
+        }
 
     // TODO: Rename and change types of parameters
     private var param1: String? = null
@@ -51,31 +70,35 @@ class creaPost : Fragment() {
         savedInstanceState: Bundle?
     ): View {
 
-        val binding: FragmentCreaPostBinding = DataBindingUtil.inflate(
+        binding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_crea_post, container, false
         )
 
 
-        binding.floatingActionButton.setOnClickListener{view : View ->
-            if(binding.tituloEditText.text.isNotEmpty()
+        binding.floatingActionButton.setOnClickListener { view: View ->
+            if (binding.tituloEditText.text.isNotEmpty()
                 && binding.descripcionEditText.text.isNotEmpty()
-                && binding.timeEditText.text.isNotEmpty()){
+                && binding.timeEditText.text.isNotEmpty()
+            ) {
 
-                    db.collection("Donaciones").add(hashMapOf(
+                db.collection("Donaciones").add(
+                    hashMapOf(
                         "titulo" to binding.tituloEditText.text.toString(),
                         "descripcion" to binding.descripcionEditText.text.toString(),
                         "fecha" to binding.timeEditText.text.toString()
 
-                    ))
-                view.findNavController().navigate(creaPostDirections.actionCreaPostToFragmentDonaciones())
+                    )
+                )
+                view.findNavController()
+                    .navigate(creaPostDirections.actionCreaPostToFragmentDonaciones())
             } else {
                 showAlert()
             }
 
         }
 
-        binding.addImage.setOnClickListener{ view: View ->
-            ponerImagen(view)
+        binding.addImage.setOnClickListener { view: View ->
+            abrirCamara()
         }
         return binding.root
     }
@@ -90,14 +113,14 @@ class creaPost : Fragment() {
         val data = baos.toByteArray()
 
         val uploadTask = pathReference.putBytes(data)
-        uploadTask.addOnFailureListener{
+        uploadTask.addOnFailureListener {
             Snackbar.make(view, R.string.error, Snackbar.LENGTH_LONG).show()
         }.addOnSuccessListener {
             Snackbar.make(view, R.string.success, Snackbar.LENGTH_LONG).show()
         }
     }
 
-    private fun showAlert(){
+    private fun showAlert() {
         val builder = AlertDialog.Builder(context)
         builder.setTitle("Error")
         builder.setMessage("Todos los campos son obligatorios!!!")
@@ -106,6 +129,45 @@ class creaPost : Fragment() {
         dialog.show()
     }
 
+    private val startActivityGal = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val data = result.data?.data
+
+            binding?.imageView6.setImageURI(data)
+        }
+    }
+
+    private fun abrirGaleria() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityGal.launch(intent)
+    }
+
+    private fun abrirCamara() {
+        lifecycleScope.launchWhenStarted {
+            getTempFile().let { uri ->
+                latestTempUri = uri
+
+                tomarImgResult.launch(uri)
+            }
+        }
+    }
+
+    private fun getTempFile(): Uri? {
+        val tempFile = File.createTempFile("tmp_image_file", ".png", activity?.cacheDir).apply {
+            createNewFile()
+            deleteOnExit()
+        }
+        return activity?.let {
+            FileProvider.getUriForFile(
+                it.applicationContext,
+                "cat.copernic.donate.provider",
+                tempFile
+            )
+        }
+    }
 
 
     companion object {
