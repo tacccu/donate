39bbfc9ff592c8089d0add837078c8a2dd55591e
pedro <palgarra2@gmail.com>
2022-01-09 23:31:52 +0100

    wip recyclerview tl tickets

--- app/src/main/java/cat/copernic/donate/ui/CreateReport.kt
+++ app/src/main/java/cat/copernic/donate/ui/CreateReport.kt
@@ -5,7 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import cat.copernic.donate.R
+import cat.copernic.donate.databinding.FragmentCreateReportBinding
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,25 +23,42 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class CreateReport : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+        private var __binding: FragmentCreateReportBinding? = null
+        private val binding get() = __binding!!
+        private var db = FirebaseFirestore.getInstance()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+        override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+        ): View? {
+
+            __binding = FragmentCreateReportBinding.inflate(inflater, container, false)
+
+            return binding.root
+        }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val spinner = binding.spinnerSelecCuentaRazonReport
+
+        val user = Firebase.auth.currentUser
+        val uid = user?.uid
+        val nomUsuario = db.collection("usuarios").document(uid.toString())
+
+        spinner.adapter = context?.let {
+            ArrayAdapter(
+                it,
+                R.layout.spinner_dropdown_item,
+                resources.getStringArray(R.array.tipoReport)
+            )
+        }
+        binding.fabreport.setOnClickListener(){
+            if(binding.intrDescripReport.text.isNotEmpty()){
+                db.collection("Reportes").document().set(hashMapOf("usuario" to nomUsuario.toString() ,"tipoReporte" to binding.spinnerSelecCuentaRazonReport.selectedItem.toString() ,"descripcion" to binding.intrDescripReport.text.toString()))
+            }
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create_report, container, false)
-    }
 
     companion object {
         /**
