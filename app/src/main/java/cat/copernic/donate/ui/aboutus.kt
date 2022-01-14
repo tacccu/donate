9e2f79ee569bc3ba8cd169be6b11dc4cf9e761ab
Lucas Tolón Pachecho <tolon.pacheco.lucas@alumnat.copernic.cat>
2022-01-14 07:12:10 +0100

    fixed

--- app/src/main/java/cat/copernic/donate/ui/aboutus.kt
+++ app/src/main/java/cat/copernic/donate/ui/aboutus.kt
@@ -4,15 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.lifecycle.Observer
-import cat.copernic.donate.R
 import cat.copernic.donate.databinding.ActivityAboutusBinding
-import cat.copernic.donate.viewmodel.aboutUsViewModel
+import cat.copernic.donate.viewmodel.AboutUsViewModel
 
 class aboutus : AppCompatActivity() {
 
     private lateinit var binding : ActivityAboutusBinding
 
-    private val aboutUsViewModel : aboutUsViewModel by viewModels()
+    private val AboutUsViewModel : AboutUsViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,11 +19,11 @@ class aboutus : AppCompatActivity() {
         binding = ActivityAboutusBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        aboutUsViewModel.aboutUsModel.observe(this, Observer {
+        AboutUsViewModel.aboutUsModel.observe(this, Observer {
             binding.TitleTV.text = it.title
             binding.aboutUsTV.text = it.text
         })
 
-        aboutUsViewModel.getText()
+        AboutUsViewModel.getText()
     }
 }
