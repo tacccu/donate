a70a4fc6fc946255a19d05002db3e1a44ce46b13
pedro <algarra.delgado.pedro@alumnat.copernic.cat>
2022-01-21 08:10:03 +0100

    .

--- app/src/main/java/cat/copernic/donate/ui/LoginActivity.kt
+++ app/src/main/java/cat/copernic/donate/ui/LoginActivity.kt
@@ -3,17 +3,18 @@ package cat.copernic.donate.ui
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ProgressBar
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.ViewModelProvider
 import cat.copernic.donate.R
 import cat.copernic.donate.databinding.ActivityLoginBinding
-import cat.copernic.donate.ui.MainActivity
-import cat.copernic.donate.ui.ProviderType
-import cat.copernic.donate.ui.RegistroActivity
-import cat.copernic.donate.viewmodel.LoginActivityViewModel
+import cat.copernic.donate.ui.ReContraActivity
+import cat.copernic.donate.ui.registro.RegistroActivity
 import com.google.firebase.auth.FirebaseAuth
+import kotlinx.coroutines.*
 
 
 class LoginActivity : AppCompatActivity() {
@@ -21,15 +22,16 @@ class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
     var fbAuth = FirebaseAuth.getInstance()
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         Thread.sleep(2000)
-        //setTheme(R.style.Theme_DonAtelogin)
+        setTheme(R.style.Theme_DonAte)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
 
         super.onCreate(savedInstanceState)
 
         // Get the viewModel
-        var viewModel = ViewModelProvider(this).get(LoginActivityViewModel::class.java)
+        //var viewModel = ViewModelProvider(this).get(LoginActivityViewModel::class.java)
 
         //setup
         setup()
@@ -38,15 +40,27 @@ class LoginActivity : AppCompatActivity() {
 
     private fun setup(){
 
+        title = "Login"
+
         binding.buttonRegister.setOnClickListener {
             intent = Intent(this, RegistroActivity::class.java)
             startActivity(intent)
 
         }
+        binding.olvidadoContra.setOnClickListener {
+            intent = Intent(this, ReContraActivity::class.java)
+            startActivity(intent)
+        }
 
+        var cr : Job? = null
         binding.buttonLogIn.setOnClickListener {
             intent = Intent(this, MainActivity::class.java)
 
+            cr = create(
+                5,
+                binding.buttonLogIn,
+                binding.bar
+            )
             if(binding.emailEditText.text.isNotEmpty() && binding.passwordEditText.text.isNotEmpty() ){
                 FirebaseAuth.getInstance()
                     .signInWithEmailAndPassword(binding.emailEditText.text.toString(), binding.passwordEditText.text.toString())
@@ -58,10 +72,6 @@ class LoginActivity : AppCompatActivity() {
                         }
                     }
             }
-        }
-
-        binding.olvidadoContra.setOnClickListener {
-
         }
     }
 
@@ -76,10 +86,31 @@ class LoginActivity : AppCompatActivity() {
     private fun showAlert() {
         val builder = AlertDialog.Builder(this)
         builder.setTitle("Error")
-        builder.setMessage("Ha habido un error iniciando sesion")
+        builder.setMessage("las credenciales no coinciden")
         builder.setPositiveButton("Aceptar", null)
         val dialog: AlertDialog = builder.create()
         dialog.show()
+    }
+
+    private fun create(time: Int, but : Button, bar : ProgressBar) = GlobalScope.launch(Dispatchers.Main) {
+        bar.setVisibility(View.VISIBLE)
+        bar.progress = 0
+
+        withContext(Dispatchers.IO) {
+            var i = 0
+
+            while(i < time) {
+                if (sus((time * 50).toLong())) {
+                    i++
+                    bar.progress = (i * 50) / time
+                }
+            }
+        }
+    }
+
+    private suspend fun sus(time: Long): Boolean {
+        delay(time)
+        return true
     }
 
 }
